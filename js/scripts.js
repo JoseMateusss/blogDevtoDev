da8df004c7434c8cc04af9a40a8cbb48748b6d8e
josemateusss <josemateusp08cs@gmail.com>
2021-12-11 21:22:29 -0300

    Primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Blog Home v5.0.7 (https://startbootstrap.com/template/blog-home)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function() {
+    $html.classList.toggle('dark-mode')
+})
